396ea59e99263632d4a3b8398cdcc9c2c4562646
耗子 <i@haozi.net>
2025-03-12 23:10:50 +0800

    fix: 修改验证码规则

--- internal/http/request/verify_code.go
+++ internal/http/request/verify_code.go
@@ -6,14 +6,14 @@ import (
 
 type VerifyCodeSms struct {
 	Phone  string `json:"phone" form:"phone" validate:"required|isCnMobile"`
-	UseFor string `json:"use_for" form:"use_for" validate:"required|in:register,login,reset_password,update_phone,update_email,update_password"`
+	UseFor string `json:"use_for" form:"use_for" validate:"required|in:avatar"`
 
 	Captcha geetest.Ticket `json:"captcha" form:"captcha" validate:"required|geetest"`
 }
 
 type VerifyCodeEmail struct {
 	Email  string `json:"email" form:"email" validate:"required|email"`
-	UseFor string `json:"use_for" form:"use_for" validate:"required|in:register,login,reset_password,update_phone,update_email,update_password"`
+	UseFor string `json:"use_for" form:"use_for" validate:"required|in:avatar"`
 
 	Captcha geetest.Ticket `json:"captcha" form:"captcha" validate:"required|geetest"`
 }
