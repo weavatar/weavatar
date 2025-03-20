f1e448ef31355c2959c5c45488b5644c19cc2215
耗子 <i@haozi.net>
2025-03-20 15:27:02 +0800

    fix: 部分q头报错

--- pkg/avatars/qq.go
+++ pkg/avatars/qq.go
@@ -20,10 +20,10 @@ func Qq(qq string) ([]byte, error) {
 	}
 
 	/**
-	 * 有一部分 QQ 头像可能是因为腾讯服务器 BUG 的原因，导致在 100 清晰度下是最佳显示效果，但是在 640 清晰度下则显示出了几十分辨率的屎。
-	 * 还有部分 QQ 头没 640 尺寸的图片，这时候尝试获取 100 尺寸的。
+	 * 有一部分Q头可能是因为腾讯服务器 BUG 的原因，导致在 100 清晰度下是最佳显示效果，但是在 640 清晰度下则显示出了几十分辨率的屎。
+	 * 还有部分Q头没 640 尺寸的图片，这时候尝试获取 100 尺寸的。
 	 *
-	 * 比如：
+	 * 例如：
 	 * http://q1.qlogo.cn/g?b=qq&nk=1327444568&s=100
 	 * http://q1.qlogo.cn/g?b=qq&nk=1327444568&s=640
 	 *
@@ -44,6 +44,17 @@ func Qq(qq string) ([]byte, error) {
 		}
 		if !resp.IsSuccessState() {
 			return nil, fmt.Errorf("failed to get QQ 100 avatar: %s", resp.String())
+		}
+		/**
+		 * 2025/03/20 部分Q头在 100 尺寸下是报错的
+		 *
+		 * 例如：https://q1.qlogo.cn/g?b=qq&nk=54445660&s=100
+		 *
+		 * 所以这里先尝试用vips解析一下，如果解析失败则直接返回
+		 */
+		_, err = vips.NewImageFromBuffer(resp.Bytes())
+		if err != nil {
+			return nil, err
 		}
 	}
 
