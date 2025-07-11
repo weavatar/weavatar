4fc5125213f06c82549cca3afc34d188099f344a
耗子 <haozi@loli.email>
2025-07-11 23:08:36 +0800

    fix: carbon

--- internal/biz/app.go
+++ internal/biz/app.go
@@ -3,12 +3,12 @@ package biz
 import "github.com/dromara/carbon/v2"
 
 type App struct {
-	ID        string                             `gorm:"type:char(10);primaryKey" json:"id"`
-	UserID    string                             `gorm:"type:char(10);index;not null" json:"user_id"`
-	Name      string                             `gorm:"type:varchar(255);not null" json:"name"`
-	Secret    string                             `gorm:"type:varchar(255);not null" json:"-"`
-	CreatedAt carbon.LayoutType[carbon.DateTime] `gorm:"type:datetime;not null" json:"created_at"`
-	UpdatedAt carbon.LayoutType[carbon.DateTime] `gorm:"type:datetime;not null" json:"updated_at"`
+	ID        string          `gorm:"type:char(10);primaryKey" json:"id"`
+	UserID    string          `gorm:"type:char(10);index;not null" json:"user_id"`
+	Name      string          `gorm:"type:varchar(255);not null" json:"name"`
+	Secret    string          `gorm:"type:varchar(255);not null" json:"-"`
+	CreatedAt carbon.DateTime `gorm:"type:datetime;not null" json:"created_at"`
+	UpdatedAt carbon.DateTime `gorm:"type:datetime;not null" json:"updated_at"`
 
 	User *User `json:"-"`
 }
